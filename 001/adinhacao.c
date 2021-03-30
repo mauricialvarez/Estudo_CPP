c06e4c9a7a3bde60d86209f5c4c9bf8c54e8877d
Maurici Alvarez <maurici.alvarez@gmail.com>
2021-03-30 14:49:24 -0300

    1 parte do curso.

--- 001/adinhacao.c
+++ 001/adinhacao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("******************************************");
-    printf("* Bem-vindo ao nosso jogo de adivinhação *");
-    printf("******************************************");
+    printf("******************************************\n");
+    printf("* Bem-vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
 }
